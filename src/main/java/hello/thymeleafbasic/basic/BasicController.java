8c7801cc19227691e1c1e612f00f70470a27f33c
jongyounlee <jayylee.dev@gmail.com>
2023-09-12 11:30:19 +0900

    SpringEL

--- src/main/java/hello/thymeleafbasic/basic/BasicController.java
+++ src/main/java/hello/thymeleafbasic/basic/BasicController.java
@@ -20,4 +20,49 @@ public class BasicController {
         model.addAttribute("data", "Hello <b>Spring!</b>");
         return "basic/text-unescaped";
     }
+
+    @GetMapping("/variable")
+    public String variable(Model model) {
+
+        User userA = new User("userA", 10);
+        User userB = new User("userB", 20);
+
+        List<User> list = new ArrayList<>();
+        list.add(userA);
+        list.add(userB);
+
+        Map<String, User> map = new HashMap<>();
+        map.put("userA", userA);
+        map.put("userB", userB);
+
+        model.addAttribute("user", userA);
+        model.addAttribute("users", list);
+        model.addAttribute("userMap", map);
+
+        return "basic/variable";
+
+    }
+
+    @GetMapping("/basic-objects")
+    public String basicObjects(HttpSession session) {
+        session.setAttribute("sessionData", "Hello Session");
+        return "basic/basic-objects";
+    }
+
+    @GetMapping("/helloBean")
+    public String hello(String data) {
+        return "Hello " + data;
+    }
+
+    @Data
+    static class User {
+
+        private String username;
+        private int age;
+
+        public User(String username, int age) {
+            this.username = username;
+            this.age = age;
+        }
+    }
 }
